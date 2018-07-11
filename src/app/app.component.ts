ec53aa3ff48530abf62786fc952db6329887a2fb
JKWA <joe.koski@gmail.com>
2018-07-11 00:17:08 -0700

    add offline message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 import { ScrollEvent } from 'ngx-scroll-event';
 import { ActivatedRoute, Router } from '@angular/router';
+import {MatSnackBar} from '@angular/material';
+
 
 @Component({
   selector: 'app-root',
@@ -10,14 +12,27 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class AppComponent implements OnInit {
   bottomOffset = 1000;
   topOffset = 0;
+  online = true;
 
   constructor(
     private route: ActivatedRoute,
-    private router: Router
+    private router: Router,
+    public snackBar: MatSnackBar
   ) {}
 
+  @HostListener('window:offline', ['$event'])
+  openSnackbar(event) {
+    this.snackBar.open('No network detected', '', {
+      duration: 0,
+    });
+  }
 
-  ngOnInit() {  }
+  @HostListener('window:online', ['$event'])
+  closeSnackbar(event) {
+    this.snackBar.dismiss();
+  }
+
+  ngOnInit() { }
 
   public handleScroll(event: ScrollEvent) {
     const state = this.router.routerState;
