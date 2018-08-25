57f036fb51fb8e1912f6c90c27554cc0a5607db1
JKWA <joe.koski@gmail.com>
2018-08-25 14:28:44 -0700

    update to abstract class

--- src/app/messages/service.messages.ts
+++ src/app/messages/service.messages.ts
@@ -2,115 +2,111 @@ import { ServiceMessageModel } from '../models/service-message.model';
 import { Service } from '../enums/service.enum';
 
 
-
-class Message {
-    public  successMessage: ServiceMessageModel;
-    public  errorMessage: ServiceMessageModel;
-    constructor (_successMessage, _errorMessage) {
-        this.successMessage = _successMessage;
-        this.errorMessage = _errorMessage;
-    }
+abstract class Message {
+    abstract get successMessage(): ServiceMessageModel;
+    abstract get errorMessage(): ServiceMessageModel;
 }
 
+
 class LocalStorageMessage extends Message {
-    constructor() {
-        super(
-            {
-                status: 2000,
-                statusText: `Saved to local storage`,
-                service: Service.LocalStorage
-            },
-            {
-                status: 2100,
-                statusText: `Failed to save to local storage`,
-                service: Service.LocalStorage
-            }
-        );
+    get successMessage() {
+        return {
+            status: 2000,
+            statusText: `Saved to local storage`,
+            service: Service.LocalStorage
+        };
+    }
+    get errorMessage() {
+        return  {
+            status: 2100,
+            statusText: `Failed to save to local storage`,
+            service: Service.LocalStorage
+        };
     }
 }
 
 class GetExpiredArticlesMessage extends Message {
-    constructor() {
-        super(
-            {
-                status: 2200,
-                statusText: `Received expired articles`,
-                service: Service.IndexedDb
-            },
-            {
-                status: 2300,
-                statusText: `Failed to retrieve expired articles`,
-                service: Service.IndexedDb
-            }
-        );
+    get successMessage() {
+        return {
+            status: 2200,
+            statusText: `Received expired articles`,
+            service: Service.IndexedDb
+        };
+    }
+    get errorMessage() {
+        return{
+            status: 2300,
+            statusText: `Failed to retrieve expired articles`,
+            service: Service.IndexedDb
+        };
     }
 }
 
 class DeletedArticlesMessage extends Message {
-    constructor() {
-        super(
-            {
-                status: 1100,
-                statusText: `Saved articles`,
-                service: Service.IndexedDb
-            },
-            {
-                status: 1200,
-                statusText: `Failed to save articles`,
-                service: Service.IndexedDb
-            }
-        );
+    get successMessage() {
+        return {
+            status: 1100,
+            statusText: `Saved articles`,
+            service: Service.IndexedDb
+        };
+    }
+    get errorMessage() {
+        return {
+            status: 1200,
+            statusText: `Failed to save articles`,
+            service: Service.IndexedDb
+        };
     }
 }
 
 class SavedIndexedDbMessage extends Message {
-    constructor() {
-        super(
-            {
-                status: 1200,
-                statusText: `Deleted expired articles`,
-                service: Service.IndexedDb
-            },
-            {
-                status: 1300,
-                statusText: `Failed to delete expired articles`,
-                service: Service.IndexedDb
-            }
-        );
+    get successMessage() {
+        return {
+            status: 1200,
+            statusText: `Deleted expired articles`,
+            service: Service.IndexedDb
+        };
+    }
+    get errorMessage() {
+        return {
+            status: 1300,
+            statusText: `Failed to delete expired articles`,
+            service: Service.IndexedDb
+        };
     }
 }
 
 class NewsApiMessage extends Message {
-    constructor() {
-        super(
-            {
-                status: 100,
-                statusText: `Received articles`,
-                service: Service.NewsAPI
-            },
-            {
-                status: 200,
-                statusText: `Failed to receive articles`,
-                service: Service.NewsAPI
-            }
-        );
+    get successMessage() {
+        return {
+            status: 100,
+            statusText: `Received articles`,
+            service: Service.NewsAPI
+        };
+    }
+    get errorMessage() {
+        return {
+            status: 200,
+            statusText: `Failed to receive articles`,
+            service: Service.NewsAPI
+        };
     }
 }
 
 class CurrentAppStatus extends Message {
-    constructor() {
-        super(
-            {
-                status: 1,
-                statusText: `Received articles`,
-                service: Service.AppStatus
-            },
-            {
-                status: 2,
-                statusText: `Failed to receive articles`,
-                service: Service.AppStatus
-            }
-        );
+    get successMessage() {
+        return {
+            status: 1,
+            statusText: `Received articles`,
+            service: Service.AppStatus
+        };
+    }
+    get errorMessage() {
+        return {
+            status: 2,
+            statusText: `Failed to receive articles`,
+            service: Service.AppStatus
+        };
     }
 }
 
